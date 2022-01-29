428bab7bf993a6f9888896df017423faf5d1a427
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-01-29 15:35:17 +0330

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="\">Amazona</a>
+      </header>
+      <main>Products List</main>
+    </div>
+  );
+}
+
+export default App;
