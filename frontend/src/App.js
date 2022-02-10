9f3af8577002ebf85d55eaf05d2f8c260c63b2e6
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-02-10 08:30:08 +0330

    added signin screen ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
+import SigninScreen from './screens/signinScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -41,6 +42,7 @@ function App() {
               <Route path="/" element={<HomeScreen />} />
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
             </Routes>
           </Container>
         </main>
