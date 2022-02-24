e7fd4769593765e1a594019beeabf8faab302fe6
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-02-24 08:38:57 +0330

    created signup screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import SigninScreen from './screens/signinScreen';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ShippingAddressScreen from './screens/shippingAddressScreen';
+import SignupScreen from './screens/signupScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -76,6 +77,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
             </Routes>
           </Container>
